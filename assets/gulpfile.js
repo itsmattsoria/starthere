b1161c6f869702a9cf9b2bf06dea76f51d832e7b
Matt Soria <matt.m.soria@gmail.com>
2016-05-19 11:52:06 -0500

    Updating gulpfile to include browsersync, separating a build task and watch task, updating jquery

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -5,13 +5,13 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    notify = require('gulp-notify'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
+    runSequence  = require('run-sequence'),
     imagemin = require('gulp-imagemin'),
     concat = require('gulp-concat'),
-    livereload = require('gulp-livereload'),
+    browserSync = require('browser-sync').create(),
     svgstore = require('gulp-svgstore'),
     svgmin = require('gulp-svgmin'),
     svg2png = require('gulp-svg2png')
@@ -23,6 +23,13 @@ var paths = {
     svgSrc: 'svgs/*.svg'
 }
 
+// Setup browsersync
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "starthere.static"
+    });
+});
+
 // Do the stuff!
 
 // Sass compilation and output
@@ -33,7 +40,7 @@ gulp.task('styles', function() {
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
     .pipe(gulp.dest('css'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(browserSync.stream());
 });
 
 // Javascript concatenation
@@ -46,7 +53,7 @@ gulp.task('scripts', function() {
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest('js/build'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+    .pipe(browserSync.stream());
 });
 
 // Compress images (must run glulp images manually)
@@ -54,7 +61,7 @@ gulp.task('images', function() {
   return gulp.src(paths.imgSrc)
     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
     .pipe(gulp.dest('images'))
-    .pipe(notify({ message: 'Images task complete' }));
+    .pipe(browserSync.stream());
 });
 
 // SVG time!
@@ -71,6 +78,7 @@ gulp.task('svgs', function() {
     .pipe(svgstore({ inlineSvg: true }))
     .pipe(rename({suffix: '-defs'}))
     .pipe(gulp.dest('svgs/build'))
+    .pipe(browserSync.stream());
 });
 // convert to png for fallback
 gulp.task('svgfallback', function() {
@@ -79,20 +87,33 @@ gulp.task('svgfallback', function() {
     .pipe(gulp.dest('../'));
 });
 
+// Do the build
+gulp.task('build', function(callback) {
+  runSequence('styles',
+              'scripts',
+              ['images', 'svgs', 'svgfallback'],
+              callback);
+});
+
 // Gulp watch
 gulp.task('watch', function() {
+  // Init BrowserSync
+  browserSync.init({
+    files: ['*.html', '*.php'],
+    proxy: 'starthere.static',
+    notify: false,
+  });
+  // Kick it off with a build
+  gulp.start('build');
   // Watch sass files
   gulp.watch(paths.scssSrc, ['styles']);
   // Watch js files
   gulp.watch(['js/libs/*.js', 'js/main.js'], ['scripts']);
   // Watch SVGs
   gulp.watch(paths.svgSrc, ['svgs', 'svgfallback']);
-  // LiveReload
-  livereload.listen();
-  gulp.watch(['assets/**']).on('change', livereload.changed);
 });
 
 // Make watch the default task
 gulp.task('default', function() {
-    gulp.start('watch');
+    gulp.start('build');
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,98 @@
+var Main = (function($) {
 
-var Main = (function ($) {
-	return {
-		myFunction: function () {
+  var screen_width = 0,
+      breakpoint_small = false,
+      breakpoint_medium = false,
+      breakpoint_large = false,
+      breakpoint_array = [480,1000,1200],
+      $document,
+      loadingTimer;
 
-		},
-		initMain: function () {
-			$(document).ready(function () {
-				Main.myFunction();
-			});
-		}
-	};
-// Pass in jQuery.
+  function _init() {
+    // touch-friendly fast clicks
+    FastClick.attach(document.body);
+
+    // Cache some common DOM queries
+    $document = $(document);
+    $('body').addClass('loaded');
+
+    // Set screen size vars
+    _resize();
+
+    // Init functions
+    _testInit();
+
+    // Esc handlers
+    $(document).keyup(function(e) {
+      if (e.keyCode === 27) {
+
+      }
+    });
+
+    // Smoothscroll links
+    $('a.smoothscroll').click(function(e) {
+      e.preventDefault();
+      var href = $(this).attr('href');
+      _scrollBody($(href));
+    });
+
+    // Scroll down to hash afer page load
+    $(window).load(function() {
+      if (window.location.hash) {
+        _scrollBody($(window.location.hash)); 
+      }
+    });
+
+  } // end init()
+
+  function _scrollBody(element, duration, delay) {
+    if ($('#wpadminbar').length) {
+      wpOffset = $('#wpadminbar').height();
+    } else {
+      wpOffset = 0;
+    } 
+    element.velocity("scroll", {
+      duration: duration,
+      delay: delay,
+      offset: -wpOffset
+    }, "easeOutSine");
+  }
+
+  function _testInit() {
+    console.log('Testing testing...is this thing on?');
+  }
+
+  // Track ajax pages in Analytics
+  function _trackPage() {
+    if (typeof ga !== 'undefined') { ga('send', 'pageview', document.location.href); }
+  }
+
+  // Track events in Analytics
+  function _trackEvent(category, action) {
+    if (typeof ga !== 'undefined') { ga('send', 'event', category, action); }
+  }
+
+  // Called in quick succession as window is resized
+  function _resize() {
+    screenWidth = document.documentElement.clientWidth;
+    breakpoint_small = (screenWidth > breakpoint_array[0]);
+    breakpoint_medium = (screenWidth > breakpoint_array[1]);
+    breakpoint_large = (screenWidth > breakpoint_array[2]);
+  }
+
+  // Public functions
+  return {
+    init: _init,
+    resize: _resize,
+    scrollBody: function(section, duration, delay) {
+      _scrollBody(section, duration, delay);
+    }
+  };
+
 })(jQuery);
 
-Main.initMain();
+// Fire up the mothership
+jQuery(document).ready(Main.init);
+
+// Zig-zag the mothership
+jQuery(window).resize(Main.resize);
